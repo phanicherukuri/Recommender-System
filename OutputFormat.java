999ff9626a5261807049e73078138ae454838a17
Phani Cherukuri <cherukuriphanichowdary@gmail.com>
2017-05-24 16:42:46 -0500

    Add files via upload

---
+++ OutputFormat.java
@@ -0,0 +1,24 @@
+package com.mkyong.rest;
+
+public class OutputFormat {
+	private long item;
+	private float value;
+	public long getItem() {
+		return item;
+	}
+	public void setItem(int item) {
+		this.item = item;
+	}
+	public float getValue() {
+		return value;
+	}
+	public void setValue(float value) {
+		this.value = value;
+	}
+	public OutputFormat(long item, float value) {
+		this.item = item;
+		this.value = value;
+	}
+	
+	
+}
